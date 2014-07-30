3290adea88c32dd2a2ee0d585dd96cbad3b9bb2b
Ronald Mannak <ronaldmannak@me.com>
2014-07-29 21:01:55 -0700

    Updated Hole1

--- SwiftGolf/Golf.swift
+++ SwiftGolf/Golf.swift
@@ -10,32 +10,50 @@ import Foundation
 
 class Golf {
 
-    class func hole1(i: Int) -> Int {
-        var f = 1
-        for j in 1 ... i {
-            f *= j
+//    class func hole1(i: Int) -> Int {
+//        var f = 1
+//        for j in 1 ... i {
+//            f *= j
+//        }
+//        return f
+//    }
+    
+    class func hole1(a: [Int]) -> Int {
+        var j = 1
+        for i in a {
+            j *= i
         }
-        return f
+        return j
     }
     
     class func hole2() -> [String] {
+        var u = 65 // A in Unicode
+        var a:[String] = Array()
+        var c:Character = "A"
+        while (c != "Z") {
+            let s = "\u\(u)}"
+            let c: Character = s
+            a.append(c)
 
+        }
+        
+        return a
     }
     
     class func hole3(i: Int) -> Int {
-        
+        return 0
     }
     
     class func hole4(i:Int) -> [String] {
-
+        return [""]
     }
     
     class func hole5(i:Int) -> [Int] {
-        
+        return [0]
     }
     
     class func hole6(s:String) -> String {
-
+        return ""
     }
     
     class func hole7() {
